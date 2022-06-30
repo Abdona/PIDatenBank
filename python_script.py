9a85622bfad01cfa926ec8a2df6bffe09fae14fb
David Ermakov <62952539+Goldmagination@users.noreply.github.com>
2022-06-30 01:01:55 -0700

    works

--- python_script.py
+++ python_script.py
@@ -3,15 +3,20 @@ import psycopg2
 
   
   
+
+
+
+
+
+
 def create_tables():
-    """ create tables in the PostgreSQL database"""
     commands = (
         """
         CREATE TABLE companies (
             company_id SERIAL PRIMARY KEY,
             company_name VARCHAR(255) NOT NULL,
             company_description VARCHAR(255) NOT NULL,
-            company_email NVARCHAR(320) NOT NULL,
+            company_email VARCHAR(320) NOT NULL
         )
         """,
         """ CREATE TABLE addresses (
@@ -19,36 +24,36 @@ def create_tables():
                 latitude DECIMAL (2,2),
                 longitude DECIMAL (2,2),
                 city VARCHAR(50) NOT NULL,
-                address VARCHAR(255) NOT NULL,
+                address VARCHAR(255) NOT NULL
                 )
         """,
         """ CREATE TABLE service_centers (
                 service_center_id SERIAL PRIMARY KEY,
                 grade_name VARCHAR(255) NOT NULL,
-                FOREIGN KEY (company_id),
-                FOREIGN KEY (address_id)
+                companyID integer REFERENCES companies(company_id),
+                addressID integer REFERENCES addresses(address_id)
                 )
         """,
-        """ CREATE TABLE category (
+        """ CREATE TABLE categories (
                 category_id SERIAL PRIMARY KEY,
                 category_name VARCHAR(20) NOT NULL,
-                FOREIGN KEY (service_center_id),
+                service_centerID integer REFERENCES service_centers(service_center_id)
                 )
         """,
-        """ CREATE TABLE service (
+        """ CREATE TABLE services (
                 service_id SERIAL PRIMARY KEY,
                 service_description VARCHAR(120) NOT NULL,
-                FOREIGN KEY (category_id),
-                FOREIGN KEY (device_id)
+                categoryID integer REFERENCES categories(category_id),
+                device VARCHAR(50),
+                price decimal(2,1) NOT NULL
                 )
         """,
-        
         """
-        CREATE TABLE user (
+        CREATE TABLE users (
                 user_id INTEGER PRIMARY KEY,
                 first_name VARCHAR(25) NOT NULL,
                 last_name VARCHAR(25) NOT NULL,
-                email VARCHAR(25) NOT NULL,
+                email VARCHAR(25) NOT NULL
         )
         """,
         """
@@ -56,24 +61,26 @@ def create_tables():
             review_id INTEGER PRIMARY KEY,
             review_text VARCHAR(50) NOT NULL,
             rating decimal(2,1) CONSTRAINT chk_rating CHECK (rating >= 0 AND rating <= 5),
-            FOREIGN KEY (user_id),
-            FOREIGN KEY (service_center_id),
+            userID integer REFERENCES users(user_id),
+            service_centerID integer REFERENCES service_centers(service_center_id)
         )
         """,
         """
         CREATE TABLE orders (
-            FOREIGN KEY (user_id),
-            FOREIGN KEY (service_id),
+            userID integer REFERENCES users(user_id),
+            service_centerID integer REFERENCES service_centers(service_center_id),
+            serviceID integer REFERENCES services(service_id),
             price decimal(2,1) NOT NULL
         )
         """
         )
     conn = None
     try:
-        conn = psycopg2.connect("host=127.16.5.15 dbname=postgres user=postgres password=test")
+        conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=7741domkRAT.")
         cur = conn.cursor()
         for command in commands:
             cur.execute(command)
+            conn.commit()
         cur.close()
         conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -83,5 +90,10 @@ def create_tables():
             conn.close()
   
   
+
+
+
+
+
 if __name__ == '__main__':
     create_tables()
